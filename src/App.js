b9b95737b7bec9450230737a20b21fd9ae2a1b4f
Demetrios Kalcassinas <disinas@icloud.com>
2018-07-21 12:35:28 +0300

    feat: Search for new books to insert to bookshelf feature.
    The feature of search books implemented using the backend API of books.
    After application loaded all books is pre-loaded to be ready for searching.
    When a search query is given by the user the app is finding the desired books
    and showing to the user the findings.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,114 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListOfBooks from './ListOfBooks'
 
+/**
+ * TODO: Instead of using this state variable to keep track of which page
+ * we're on, use the URL in the browser's address bar. This will ensure that
+ * users can use the browser's back and forward buttons to navigate between
+ * pages, as well as provide a good URL they can bookmark and share.
+ */
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: [],
+    query: '',
+    showingBooks: []
+  }
+
+ /** Component lifecycle event method is used to hook of get-all-books method
+  * immediately after the component is inserted into the DOM and rendered
+  */
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
+  /** Managing input state by checking if there is a query,
+  * using Books API search method that returns a promise response with then
+  * and change the showing books to the user
+  */
+  updateQuery = (query) => {
+    this.setState({query: query})
+    let showingBooks = []
+    if (query) {
+      BooksAPI.search(query).then(response => {
+        if (response.length) {
+          showingBooks = response.map(b => {
+            const index = this.state.books.findIndex(c => c.id === b.id)
+            if( index >= 0 ) {
+              return this.state.books[index]
+            } else {
+              return b
+            }
+          })
+        }
+        this.setState({showingBooks})
+      })
+    }
+    else {
+      this.setState({showingBooks})
+    }
   }
 
   render() {
-    return ( <div className="app">
-      <Route exact path="/search" render = { () => ( <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to="/">Close</Link>
-          <div className="search-books-input-wrapper">
-            {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    const {query} = this.state
+    return (
+      <div className="app">
 
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-            <input type="text" placeholder="Search by title or author"/>
+          <Route exact path="/search" render={() => (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <Link className="close-search" to="/">Close</Link>
+                <div className="search-books-input-wrapper">
+                  <input type="text"
+                         placeholder="Search by title or author"
+                         value={query}
+                         onChange={(event) => this.updateQuery(event.target.value)}
+                  />
 
-          </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>
-      )} />
-      <Route exact path="/" render = { () => ( <ListOfBooks/> ) } />
-    </div> )
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid">
+                  {this.state.showingBooks.map((book, i) => (
+                    <li key={i}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{
+                            width: 128,
+                            height: 192,
+                            backgroundImage: book.imageLinks ?
+                              `url(${book.imageLinks.thumbnail})` : ''
+                          }}></div>
+                          <div className="book-shelf-changer">
+                            <select onChange={this.changeBookShelf} value={this.state.shelf}>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading
+                              </option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors ? book.authors.toString() : ' '}</div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          )} />
+          <Route exact path="/" render={() => (
+            <ListOfBooks/>
+          )}/>
+
+      </div>
+    )
   }
 }
 
